e81efb9ded679523af4a012aedd712b4ed2b7824
Arun23592 <arunsubramani1992@gmail.com>
2025-01-07 22:45:04 +0530

    pages

---
+++ tests/pages/basepage.ts
@@ -0,0 +1,11 @@
+export default class BasePage {
+  readonly page: Page;
+
+  constructor(page: Page) {
+    this.page = page;
+  }
+
+  async navigate(url: string) {
+    await this.page.goto(url);
+  }
+}
