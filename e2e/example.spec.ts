4432e7b4b24bb89ae515797b329870aabe066d49
arunsubramani <arunsubramani1992@gmail.com>
2025-05-10 23:53:06 +0530

    spec

--- e2e/example.spec.ts
+++ e2e/example.spec.ts
@@ -15,4 +15,8 @@ test('get started link', async ({ page }) => {
 
   // Expects page to have a heading with the name of Installation.
   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+
+  const getStarted = page.getByRole('link', { name: 'Get Started'});
+
+  await getStarted.click();
 });
