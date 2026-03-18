5ac0f75984cb9186100e6194ffcad752a6f1ac3e
arunsubramani <arunsubramani1992@gmail.com>
2026-03-18 23:02:41 +0530

    play

--- playwright.config.js
+++ playwright.config.js
@@ -16,13 +16,56 @@ const config =  ({
   reporter : 'html',
 
   
-  use: {
+  // use: {
     
-      browserName : 'chromium'
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-  },
+  //     browserName : 'chromium'
+  //   /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+  // },
 
- 
+  projects: [
+    {
+      name: 'chromium',
+      use: { 
+        ...devices['Desktop Chrome'],
+        viewport:{width:1920, height:8080}
+        
+       },
+    },
+
+    // {
+    //   name: 'firefox',
+    //   use: { 
+    //     ...devices['Desktop Firefox'] },
+    //     viewport:{width:1512, height:803}
+    // },
+
+    // {
+    //   name: 'webkit',
+    //   use: { 
+    //     ...devices['Desktop Safari'] },
+    //     viewport:{width:1512, height:803}
+    // },
+
+    /* Test against mobile viewports. */
+    // {
+    //   name: 'Mobile Chrome',
+    //   use: { ...devices['Pixel 5'] },
+    // },
+    // {
+    //   name: 'Mobile Safari',
+    //   use: { ...devices['iPhone 12'] },
+    // },
+
+    /* Test against branded browsers. */
+    // {
+    //   name: 'Microsoft Edge',
+    //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
+    // },
+    // {
+    //   name: 'Google Chrome',
+    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
+    // },
+  ],
 
 
 });
